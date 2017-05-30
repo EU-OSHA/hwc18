91e64f06ce0f1587974a26c4c977b908f7ede410
Octavian Belu <beluoctavian@gmail.com>
2017-05-30 15:29:03 +0300

    Newsletter templates files

--- docroot/sites/all/themes/hwc_frontend/templates/taxonomy-term--newsletter_sections.tpl.php
+++ docroot/sites/all/themes/hwc_frontend/templates/taxonomy-term--newsletter_sections.tpl.php
@@ -1,7 +1,7 @@
 <table border="0" cellpadding="10" cellspacing="0" class="category-name" width="100%">
   <tbody>
     <tr>
-      <td style="font-family: Oswald, Arial, sans-serif; font-weight: bold; font-size: 20px; padding-left: 0px; padding-right: 0px; padding-top: 0px; color: #749b00;">
+      <td style="font-family: Oswald, Arial, sans-serif; font-weight: normal; font-size: 20px; padding-left: 0px; padding-right: 0px; padding-top:5px;">
         <?php
           $label = "";
           if (isset($name_field[$language])) {
@@ -12,7 +12,25 @@
             print($label);
           }
         ?>
+        <?php
+          if ($label == 'Blog') {
+            $directory = drupal_get_path('module','osha_newsletter');
+            print(theme('image', array(
+              'path' => $directory . '/images/blog-callout.png',
+              'width' => 18,
+              'height' => 15,
+              'alt' => 'callout',
+              'attributes' => array('style' => 'border: 0px;')
+            )));
+          }
+        ?>
       </td>
+    </tr>
+    <tr>
+      <td style="border-bottom:2px dotted #CFDDEE;padding-top:0px;"></td>
+    </tr>
+    <tr>
+      <td style="padding-top: 0px; padding-bottom: 10px;" class="space-beyond-dotted-line"></td>
     </tr>
   </tbody>
 </table>
