afa5bd18f6b6ee2f8bb555051a76e5cfc72878a4
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2017-06-02 14:30:14 +0300

    Priority areas module and related content types and blocks

---
+++ docroot/sites/all/modules/hwc/hwc_priority_areas/templates/hwc_priority_areas_links_and_info.tpl.php
@@ -0,0 +1,2 @@
+<button class="btn-1-normal" onclick="window.location.href='<?php echo $link; ?>'"> <?php echo $text; ?> </button>
+<div class="recomended-resources-for-you"><?php echo $recommended; ?></div>
