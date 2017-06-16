ee98551ea532702cfd7d3706ca6c7917fd874ba8
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2017-06-16 11:41:05 +0300

    enable twitter images

--- docroot/sites/all/modules/hwc/social_dashboard/templates/embedded-twitter-tweet.tpl.php
+++ docroot/sites/all/modules/hwc/social_dashboard/templates/embedded-twitter-tweet.tpl.php
@@ -1,2 +1,2 @@
-<?php
-echo '<blockquote data-cards="hidden" class="twitter-tweet" data-lang="en"><a href="' . "https://twitter.com/" . $content['name'] . "/status/" . $content['social_id'] . '"> </a></blockquote>';
+<?php //data-cards="hidden"  - hides images...
+echo '<blockquote class="twitter-tweet" data-lang="en"><a href="' . "https://twitter.com/" . $content['name'] . "/status/" . $content['social_id'] . '"> </a></blockquote>';
