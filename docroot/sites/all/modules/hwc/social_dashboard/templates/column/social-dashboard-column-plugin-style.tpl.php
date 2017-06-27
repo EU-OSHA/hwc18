caf0c964fcd903956713961111dcd7fb3291f83e
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2017-06-27 13:08:46 +0300

    social dashboard class changes

--- docroot/sites/all/modules/hwc/social_dashboard/templates/column/social-dashboard-column-plugin-style.tpl.php
+++ docroot/sites/all/modules/hwc/social_dashboard/templates/column/social-dashboard-column-plugin-style.tpl.php
@@ -8,8 +8,16 @@
  *
  * @ingroup views_templates
  */
+$one_column = FALSE;
+$in_block = TRUE;
+if (@$_REQUEST['source']) {
+  $one_column = TRUE;
+}
+if ($current_display != 'block_1') {
+  $in_block = FALSE;
+}
 ?>
-<div<?php if (@$_REQUEST['source']) { echo ' class="container"'; } ?>>
+<div<?php if ($one_column) { echo ' class="container"'; } ?>>
 <script src="//connect.facebook.net/en_US/sdk.js#xfbml=1&amp;version=v2.5" async></script>
 <script>window.twttr = (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0],
@@ -29,9 +37,14 @@
     }(document, "script", "twitter-wjs"));</script>
 <div id="social-dashboard-column-<?php print $id ?>" class="row <?php print $classes ?>">
 <?php
-    foreach ($items as $title => $rows) { ?>
-    <div <?php if ($current_display != 'block_1') { ?>class="col-xs-12 col-sm-12 col-md-4<?php echo ' ' . strtolower($title) ?>-column"<?php } ?>>
-        <?php if ($current_display != 'block_1') { ?><h2><?php echo ucfirst($title) ?></h2><?php } ?>
+    foreach ($items as $title => $rows) {
+      $col_md = 4;
+      if ($one_column) {
+        $col_md = 12;
+      }
+        ?>
+    <div <?php if (!$in_block) { ?>class="col-xs-12 col-sm-12 col-md-<?php echo $col_md . ' ' . strtolower($title) ?>-column"<?php } ?>>
+        <?php if (!$in_block) { ?><h2><?php echo ucfirst($title) ?></h2><?php } ?>
 <?php
       foreach ($rows as $row) {
           foreach ($row['content'] as $column) {
