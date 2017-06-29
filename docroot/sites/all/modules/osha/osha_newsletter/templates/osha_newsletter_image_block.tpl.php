551673ef9c8430d41974861065f472a73b9d6851
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2017-06-29 15:17:29 +0300

    HW18-54 Development of good practice exchange

--- docroot/sites/all/modules/osha/osha_newsletter/templates/osha_newsletter_image_block.tpl.php
+++ docroot/sites/all/modules/osha/osha_newsletter/templates/osha_newsletter_image_block.tpl.php
@@ -4,12 +4,6 @@
 			'path' => $background_image_url,
 			'alt' => 'Join OSHMail subscribers',
 		));
-	} else {
-		$theme_path = drupal_get_path('theme', variable_get('theme_default', 'osha_frontend'));
-		print theme('image', array(
-			'path' => $theme_path . '/images/content/blocks/join_65000_subscribers.png',
-			'alt' => 'Join OSHMail subscribers',
-		));
 	}?>
 	<div id="block_newsletter_image_text">
 		<?php print $start_text . ' ' . $subscribers_no . ' ' . $end_text?>
