a78937de18873ba178555a7698519117490273b8
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2017-09-05 23:00:53 +0300

    HW18-180 AA Accessibility Known problems

--- docroot/sites/all/modules/hwc/social_dashboard/templates/embedded-twitter-tweet.tpl.php
+++ docroot/sites/all/modules/hwc/social_dashboard/templates/embedded-twitter-tweet.tpl.php
@@ -3,4 +3,5 @@ $data_cards = '';
 if (drupal_is_front_page()) {
   $data_cards = 'data-cards="hidden"';
 }
-echo '<blockquote class="twitter-tweet" ' . $data_cards . ' data-lang="en"><a href="' . "https://twitter.com/" . $content['name'] . "/status/" . $content['social_id'] . '"> </a></blockquote>';
+echo '<blockquote class="twitter-tweet" ' . $data_cards . ' data-lang="en">
+<a href="' . "https://twitter.com/" . $content['name'] . "/status/" . $content['social_id'] . '" title="' . $content['name'] . '"> </a></blockquote>';
