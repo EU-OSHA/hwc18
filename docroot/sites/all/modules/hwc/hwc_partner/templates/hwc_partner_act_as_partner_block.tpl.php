f8d4238c9478554616a34d0e1ff45c4b53895fa1
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2017-09-14 21:52:37 +0300

    HW18-192 Development Partner resources

--- docroot/sites/all/modules/hwc/hwc_partner/templates/hwc_partner_act_as_partner_block.tpl.php
+++ docroot/sites/all/modules/hwc/hwc_partner/templates/hwc_partner_act_as_partner_block.tpl.php
@@ -1,4 +1,7 @@
-<a class="btn btn-primary btn-lg" data-toggle="modal" data-target="#act-as-partner">
+<?php
+global $language;
+?><a class="btn btn-primary btn-lg button-left" href="/<?php echo $language->language;?>/private"><?php print t('Partner resources'); ?></a>
+<a class="btn btn-primary btn-lg button-right" data-toggle="modal" data-target="#act-as-partner">
   <?php print t('Act as a partner'); ?>
 </a>
 <div id="act-as-partner" class="modal fade" tabindex="-1" role="dialog">
