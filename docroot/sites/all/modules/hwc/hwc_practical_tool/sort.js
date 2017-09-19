0b2f5a8772a5f228a945416fdd8ad9c3ffa676da
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2017-09-19 22:51:06 +0300

    hwc18-221, hwc18-222, hwc18-223

--- docroot/sites/all/modules/hwc/hwc_practical_tool/sort.js
+++ docroot/sites/all/modules/hwc/hwc_practical_tool/sort.js
@@ -8,9 +8,12 @@
 
     Drupal.behaviors.search_sort = {
         attach: function(context, settings) {
-
             jQuery('#edit-sort-by').change(function(){
                 jQuery('#views-exposed-form-practical-tools-page #edit-sort-by--2').val(jQuery(this).val());
+                jQuery('#views-exposed-form-practical-tools-page').submit();
+            });
+            jQuery('#edit-sort-order').change(function(){
+                jQuery('#views-exposed-form-practical-tools-page #edit-sort-order--2').val(jQuery(this).val());
                 jQuery('#views-exposed-form-practical-tools-page').submit();
             });
             if (jQuery('#hwc-practical-tool-sort-form').length) {
