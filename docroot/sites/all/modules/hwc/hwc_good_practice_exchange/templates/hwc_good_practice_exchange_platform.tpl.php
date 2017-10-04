760d0d58165ba668e51478abac449d73bb3c79ce
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2017-10-04 10:05:53 +0300

    HW18-191 Development Good Practice exchange

--- docroot/sites/all/modules/hwc/hwc_good_practice_exchange/templates/hwc_good_practice_exchange_platform.tpl.php
+++ docroot/sites/all/modules/hwc/hwc_good_practice_exchange/templates/hwc_good_practice_exchange_platform.tpl.php
@@ -1,7 +1,7 @@
 <div class="gpep-links-block">
   <div class="col-xs-12 col-sm-12">
     <div class="hwc-gpep-intro-text">
-      <span><?php print intro_text; ?></span>
+      <span><?php print $intro_text; ?></span>
     </div>
     <div class="hwc-gpep-links">
       <?php print implode('', $links); ?>
