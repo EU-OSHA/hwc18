0aabc5c6c3a1b7180e3159aff0cd3be7e4c85912
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2017-10-17 09:15:31 +0300

    HW18-216 Publications section carrousel

--- docroot/sites/all/themes/hwc_frontend/templates/views-view--publications-slideshow--block.tpl.php
+++ docroot/sites/all/themes/hwc_frontend/templates/views-view--publications-slideshow--block.tpl.php
@@ -1,43 +1,3 @@
-<?php if (!isset($_REQUEST['test'])) { ?>
-<script>
-jQuery(document).ready(function ($) {
-    var options = {
-        $AutoPlay: false,
-        $AutoPlaySteps: 1,
-        $SlideDuration: 160,
-    //    $SlideWidth: 800,
-        $SlideHeight: 230,
-        $SlideSpacing: 1,
-        $DisplayPieces: 1,
-        $HWA: false,
-        $BulletNavigatorOptions: {
-            $Class: $JssorBulletNavigator$,
-            $ChanceToShow: 1,
-            $AutoCenter: 1
-        },
-        $ArrowNavigatorOptions: {
-            $Class: $JssorArrowNavigator$,
-            $ChanceToShow: 1,
-            $AutoCenter: 2,
-            $Steps: 1
-        }
-    };
-    new $JssorSlider$("publications_slideshow", options);
-});
-</script>
-<div id="publications_slideshow">
-    <div id="num_slides" data-u="slides" style="">
-      <?php print $rows ?>
-    </div>
-    <div data-u="navigator" class="jssorb03">
-        <div class="prototype" data-u="prototype"></div>
-    </div>
-    <span data-u="arrowleft" class="jssora03l publications"></span>
-    <span data-u="arrowright" class="jssora03r publications"></span>
-</div>
-<?php
-}
-else { ?>
 <div id="publications_slideshow" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
     <ol class="carousel-indicators jssorb03"><?php
@@ -65,5 +25,3 @@ else { ?>
         <span class="sr-only"><?php echo t('Next'); ?></span>
     </a>
 </div>
-<?php
-}
