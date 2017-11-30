e5a39a1254dd63c390927d77bde10bedeee35591
Endika Llonin <ellonin@bilbomatica.es>
2017-11-30 11:39:27 +0100

    css changes - gallery styles v1

--- docroot/sites/all/themes/hwc_frontend/js/campaigns.js
+++ docroot/sites/all/themes/hwc_frontend/js/campaigns.js
@@ -17,6 +17,33 @@ jQuery(document).ready(function() {
         setTimeout(ipad_fix_iframe_width, 5000);
     }
 
+    // Detect objectFit support
+	//if('objectFit' in document.documentElement.style === false) {
+	  
+	  // assign HTMLCollection with parents of images with objectFit to variable
+	  var container = document.getElementsByClassName('flickr-img-wrap');
+	  
+	  // Loop through HTMLCollection
+	  for(var i = 0; i < container.length; i++) {
+	    
+	    // Asign image source to variable
+	    var imageSource = container[i].querySelector('img').src;
+	    
+	    // Hide image
+	    container[i].querySelector('img').style.display = 'none';
+	    
+	    // Add background-size: cover
+	    container[i].style.backgroundSize = 'cover';
+	    
+	    // Add background-image: and put image source here
+	    container[i].style.backgroundImage = 'url(' + imageSource + ')';
+	    
+	    // Add background-position: center center
+	    container[i].style.backgroundPosition = 'center center';
+	  }
+	//}
+
+
 	// Hide Header on on scroll down
 	var didScroll;
 	var lastScrollTop = 0;
