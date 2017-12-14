07e7b2cb277d8117c97cd4dfd91faa5b5d7010f1
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2017-12-14 11:33:02 +0200

    HW18-273 Implementation after usability recommnedations

--- docroot/sites/all/modules/osha/osha_publication/templates/osha_publication_file_document_files.tpl.php
+++ docroot/sites/all/modules/osha/osha_publication/templates/osha_publication_file_document_files.tpl.php
@@ -6,7 +6,7 @@ foreach ($files as $f) {
     if ($document_file['fid'] == $f['fid']) {
       $ext_type = strtoupper(pathinfo($f['uri'], PATHINFO_EXTENSION)) . ' ' . $entity->field_file_title['und'][$delta]['value'];
       $file_uri = file_create_url($f['uri']);
-      $items[] = '<a href="' . $file_uri . '">' . strtoupper($entity->field_language['und'][$delta]['value']) . '</a><span class="glyphicon glyphicon-circle-arrow-down"></span>';
+      $items[] = '<a href="' . $file_uri . '">' . strtoupper($entity->field_language['und'][$delta]['value']) . '<span class="glyphicon glyphicon-circle-arrow-down"></span></a>';
     }
   }
 }
