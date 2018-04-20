cc921ad07c66c8ea85e53aa9f132be849ab53502
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2018-04-20 20:42:57 +0300

    Toolkit content sync

--- docroot/sites/all/modules/hwc/hwc_toolkit/templates/hwc_toolkit_navigation.tpl.php
+++ docroot/sites/all/modules/hwc/hwc_toolkit/templates/hwc_toolkit_navigation.tpl.php
@@ -3,6 +3,7 @@
 $prev = hwc_toolkit_nav_prev($nid);
 $next = hwc_toolkit_nav_next($nid);
 if ($prev || $next) {
+  print '<div class="col-xs-12 col-sm-12 col-md-9">';
   print '<div class="key-next-prev-buttons">';
   if ($prev) {
     print '<span class="prev-button"><a href="' . $prev['href'] . '" class="previous-button">' . t('Prev topic') . ' ' . $prev['title'] . '</a></span>';
@@ -10,5 +11,6 @@ if ($prev || $next) {
   if ($next) {
     print '<span class="next-button"><a href="' . $next['href'] . '" class="nexting-button">' . t('Next topic') . ' ' . $next['title'] . '</a></span>';
   }
+  print '</div>';
   print '</div>';
 }
