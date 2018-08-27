79957938a2d9a4a59e0effe9aace8184db6f308e
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2018-08-27 12:56:40 +0300

    MDR-2129 2 changes in the Publications section

--- docroot/sites/all/modules/osha/osha_publication/js/form.js
+++ docroot/sites/all/modules/osha/osha_publication/js/form.js
@@ -32,6 +32,7 @@
             $('#osha-publication-menu-case-studies-form, #osha-publication-menu-publications-form').once('publication_toggle_facets', function(){
                 $('.form-checkboxes.search-facet-field').hide();
                 $('#edit-field-priority-area').show();
+                $('#edit-field-publication-type').show();
                 $('.form-checkboxes.search-facet-field').has('input:checked').show();
                 $('.form-checkboxes.search-facet-field').siblings('label').on('click', function() {
                     var $checkboxes = $(this).parent().find('.form-checkboxes.search-facet-field');
